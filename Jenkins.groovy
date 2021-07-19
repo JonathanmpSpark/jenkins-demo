60d3979d93fd08473c99e728b9f107ae8fd4bd36
Jonathan <jonathan.munoz@sparktechs.com>
2021-07-18 23:20:56 -0700

    [staging]

--- Jenkins.groovy
+++ Jenkins.groovy
@@ -9,23 +9,33 @@ pipeline {
   agent any
   stages {
     stage('Getting latest changes') {
-      steps {
-        git(
-            url: 'https://github.com/JonathanmpSpark/jenkins-demo',
-            credentialsId: 'github-demo-01',
-            branch: "staging"
-        )
-        script {
-            def config = readJSON file: './version.json'
-            systemVersion = "${config.AppVersion}"
-            def digits = systemVersion.tokenize('.')
-            config['AppVersion'] = "" + digits.get(0) + "." + digits.get(1) + "." + digits.get(2) + "." + "$BUILD_NUMBER"
-            dockerLabel = "" + digits.get(0) + "." + digits.get(1) + "." + digits.get(2) + "." + "$BUILD_NUMBER"
 
-            print dockerLabel
-            // writeJSON file: './version.json', json: config
-        }
+      stage("One"){
+            sleep 1
+            echo 'hello'
       }
+
+      stage("Two"){
+            sleep 1
+            echo 'world'
+      }
+      // steps {
+      //   git(
+      //       url: 'https://github.com/JonathanmpSpark/jenkins-demo',
+      //       credentialsId: 'github-demo-01',
+      //       branch: "staging"
+      //   )
+      //   script {
+      //       def config = readJSON file: './version.json'
+      //       systemVersion = "${config.AppVersion}"
+      //       def digits = systemVersion.tokenize('.')
+      //       config['AppVersion'] = "" + digits.get(0) + "." + digits.get(1) + "." + digits.get(2) + "." + "$BUILD_NUMBER"
+      //       dockerLabel = "" + digits.get(0) + "." + digits.get(1) + "." + digits.get(2) + "." + "$BUILD_NUMBER"
+
+      //       //print dockerLabel
+      //       // writeJSON file: './version.json', json: config
+      //   }
+      // }
     }
   }
 }
