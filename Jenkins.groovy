911bdeca5e14ba1b5a2b3e4290ac7c88eb7aa5dc
Jonathan <jonathan.munoz@sparktechs.com>
2021-07-18 23:14:41 -0700

    [staging]

--- Jenkins.groovy
+++ Jenkins.groovy
@@ -27,43 +27,5 @@ pipeline {
         }
       }
     }
-    stage('Building image') {
-        steps {
-        //   script {
-        //     dockerImage = docker.build(registry + ":" + dockerLabel,"-f ./dockerfiles/Webapp/Dockerfile ./")
-        //   }
-        }
-    }
-    stage('Upload docker image to registry') {
-      steps{
-    //     script {
-    //       docker.withRegistry( '', registryCredential ) {
-    //         dockerImage.push()
-    //       }
-    //     }
-      }
-    }
-    stage('Deploy container to production') {
-        steps{
-        //     script {
-        //         def remote = [:]
-        //         remote.name = "prod"
-        //         remote.host = "13.91.53.201"
-        //         remote.allowAnyHosts = true
-        //         withCredentials([usernamePassword(credentialsId: 'prodServer', passwordVariable: 'password', usernameVariable: 'userName')]) {
-                        
-        //             remote.user = userName
-        //             remote.password = password
-        //             sshCommand remote: remote, command: 'docker pull ' + registry + ":" + dockerLabel
-        //             sshCommand remote: remote, command: 'docker container stop SGWebApp_Prod_https'
-        //             sshCommand remote: remote, command: 'docker container rm SGWebApp_Prod_https'
-        //             sshCommand remote: remote, command: 'docker run -p 443:443 -e ASPNETCORE_URLS="https://+" -e ASPNETCORE_HTTPS_PORT=443 -e ASPNETCORE_Kestrel__Certificates__Default__Password= -e ASPNETCORE_Kestrel__Certificates__Default__Path=/https/sparkgam.sparktechs.com.pfx -v ${HOME}/certificate/:/https/ --name SGWebApp_Prod_https -v webapp-user-files_prod:/app/wwwroot/Uploaded -d ' + registry + ":" + dockerLabel
-                    
-        //         }
-        //     }
-          
-        }
-        
-    }
   }
 }
